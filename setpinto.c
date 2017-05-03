cc0795d8237dea4a58d80f8e9b4200e4cd503ff3
botaviorel@gmail.com <vbota@localhost.localdomain>
2017-05-03 19:58:54 +0000

    fixed list of valid and invalid pins

--- setpinto.c
+++ setpinto.c
@@ -8,10 +8,10 @@ void executeCommand (int,int[],int);
 void handleCommand (int,char *[]);
 void printHelpText (void);
 
-int ALLVALIDPINS[28]={ 3,5,7,8,10,11,12,13,15,16,18,19,21,22,23,24,26,27,28,29,31,32,33,35,36,37,38,40 };
+int ALLVALIDPINS[28] = { 0 , 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 };
 int NOOFVALIDPINS=28;
-int ALLINVALIDPINS[10] = {4,6,9,14,17,20,25,30,34,39};
-int NOOFINVALIDPINS=10;
+int ALLINVALIDPINS[4] = { 17, 18, 19, 20 };
+int NOOFINVALIDPINS=4;
 #define SUPPORTTEXT "Use sudo ./setpinsto -help for usage information\n"
 
 void main (int argc, char *argv[])
